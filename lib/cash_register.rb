bb4165233ee2599beb72231ac9ad8cc7681b690e
Ty Bandachit <tbandachit@gmail.com>
2020-03-12 08:39:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,6 +3,7 @@ class CashRegister
   attr_accessor :item, :quantity
   def initialize(discount= 0)
     @total = 0
+    @item = []
     @discount = discount
   end
   
@@ -13,7 +14,7 @@ class CashRegister
   def add_item(itemname, price, quantity= 1)
    counter = 0 
     while counter < quantity
-      
+      @item << itemname
     end
   end 
   
