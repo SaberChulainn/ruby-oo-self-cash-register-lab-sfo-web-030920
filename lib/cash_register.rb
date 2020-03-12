1e708881ab6dec36d78892d00c9eceeff48cecd3
Ty Bandachit <tbandachit@gmail.com>
2020-03-12 08:54:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
     if @discount > 0 
     discounted = @total * @discount / 100
     @total = @total - discounted
-    return "After the discount, the total comes to $#{@total}"
+    return "After the discount, the total comes to $#{@total}."
     else
     return "There is no discount to apply."
     end
